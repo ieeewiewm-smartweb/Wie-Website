cc6eb6300d049c2c5b12b51abea96866226344aa
Deep Khatri <deepkhatri050@gmail.com>
2026-06-08 10:31:26 +0530

    data updated

--- src/data/leadership.ts
+++ src/data/leadership.ts
@@ -11,20 +11,20 @@ export const defaultMembers: LeaderData[] = [
         role: "Mentor",
         description:
             "Dean, College of Technology, Silver Oak University; IEEE Senior Member; Chairperson, Professional Activity Committee, IEEE Gujarat Section; Advisor, Silver Oak University IEEE Computer Society Student Branch Chapter; Founding Member, Silver Oak University IEEE Student Branch.",
-        image: "http://ieee.socet.edu.in/wp-content/uploads/2025/05/satviksir.jpg",
+        image: "https://ieee.socet.edu.in/wp-content/uploads/2025/05/satviksir.jpg",
     },
     {
         name: "Prof. Gaurav Tiwari",
         role: "Faculty Advisor",
         description:
             "Assistant Professor, Department of Computer Engineering, College of Technology; Advisor, Silver Oak University IEEE Women In Engineering Student Branch Affinity Group",
-        image: "http://ieee.socet.edu.in/wp-content/uploads/2025/07/gaurav-sir.png",
+        image: "https://ieee.socet.edu.in/wp-content/uploads/2025/07/gaurav-sir.png",
     },
     {
-        name: "Deshna Shah",
+        name: "Setu Madhavani",
         role: "Chairperson",
         description: "",
-        image: "http://ieee.socet.edu.in/wp-content/uploads/2026/02/19.png",
+        image: "http://ieee.socet.edu.in/wp-content/uploads/2026/04/Screenshot-2026-04-09-101642.png",
     },
     {
         name: "Milan Sehgal",
@@ -33,10 +33,10 @@ export const defaultMembers: LeaderData[] = [
         image: "http://ieee.socet.edu.in/wp-content/uploads/2026/02/20.png",
     },
     {
-        name: "Maruf Fatema Mansuri",
+        name: "Mayuri Raghvani",
         role: "Secretary",
         description: "",
-        image: "http://ieee.socet.edu.in/wp-content/uploads/2026/02/21.png",
+        image: "http://ieee.socet.edu.in/wp-content/uploads/2026/04/Screenshot-2026-04-09-101907.png",
     },
     {
         name: "Price Sabalpara",
@@ -45,10 +45,10 @@ export const defaultMembers: LeaderData[] = [
         image: "http://ieee.socet.edu.in/wp-content/uploads/2026/02/22.png",
     },
     {
-        name: "Suhani Singh",
+        name: "Deep Khatri",
         role: "Webmaster",
         description: "",
-        image: "http://ieee.socet.edu.in/wp-content/uploads/2026/02/23.png",
+        image: "http://ieee.socet.edu.in/wp-content/uploads/2026/04/Screenshot-2026-04-09-102149.png",
     },
 ];
 
